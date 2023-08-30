3c5fd5462dbd5b3c9ff0e1524d8e1513bfa9d47a
shrutika546 <84497284+shrutika546@users.noreply.github.com>
2023-08-30 12:17:42 +0530

    Add files via upload

---
+++ website.sql
@@ -0,0 +1,3 @@
+create database studentms;
+use studentms;
+create table students(Citizenship_no varchar(20),Name varchar(20),Email varchar(200),Gender varchar(20),Contact varchar(20),Depature_Date varchar(200),Address varchar(100));
